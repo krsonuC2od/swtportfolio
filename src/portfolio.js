37a071bb48ef878c4a1124d3500adda317e40381
krsonuC2od <190101120062@cutm.ac.in>
2021-11-21 21:30:10 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,434 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Sweta Kumari",
+  logo_name: "$weta.k()",
+  nickname: "swt / tstie",
+  full_name: "Sweta Kumari",
+  subTitle:
+    "I am a Student,  🔥. Always learning.",
+  resumeLink:
+    "/",
+  mail: "mailto:Sweta@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "/",
+  linkedin: "/",
+  gmail: "Sweta@gmail.com",
+  gitlab: "/",
+  facebook: "/",
+  twitter: "/",
+  instagram: "/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+       
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        {
+          skillName: "Android",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "10th",
+      subtitle: "Matric ",
+      logo_path: "ssec.png",
+      alt_name: "SSEC",
+      duration: "2016-2017",
+      descriptions: [
+        "⚡ I've done in  R K C Hight school."
+       
+      ],
+      website_link: "/",
+    },
+    {
+      title: "12th",
+      subtitle: "",
+      logo_path: "ssgandhy.png",
+      alt_name: "SSGC",
+      duration: "2017 - 2019",
+      descriptions: [
+        "⚡ I have done in SBSS College, Begusarai .",
+       
+      ],
+      website_link: "/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    
+   
+   
+    
+   
+    
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "I am Now Fresher",
+  description:
+    " I'm  study now B.Tech  , and in my experience, I know Some Programing Language .",
+  header_image_path: "experience.svg",
+  sections: [
+   
+   
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My project is now very simple.! but i Will update.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "sweta.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "/",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "CBCS Management",
+      url: "/",
+      description: "This is My 3rd Year domain Project",
+      languages: [
+        {
+          name: "Java",
+          iconifyClass: "logos-java",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "My Portfolio",
+      url: "/",
+      description:
+        " This is my Portfolio Website.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+       
+      ],
+    },
+    {
+      id: "2",
+      name: "Notification management",
+      url: "/",
+      description:
+        " Notice board problem solution(for learning purposes).",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "TypeScript",
+          iconifyClass: "logos-typescript",
+        },
+      ],
+    },
+   
+    
+     
+  
+  
+    
+    
+   
+    
+  
+   
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
